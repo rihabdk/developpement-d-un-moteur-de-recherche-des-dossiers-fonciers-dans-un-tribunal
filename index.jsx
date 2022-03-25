56988871096c558374a0862e22ae176637b5ff1a
Rihab Dkhil <82368207+rihabdk@users.noreply.github.com>
2022-03-25 19:02:27 +0100

    Delete index.jsx

--- index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Map from "./map";
-import "./App.css";
-
-ReactDOM.render(
-  <Map position={[36.81897, 10.16579]} zoom={14} />,
-  document.getElementById("root")
-);
