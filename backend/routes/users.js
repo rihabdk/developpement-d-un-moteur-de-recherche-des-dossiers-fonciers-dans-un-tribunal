1989b7798bd62e0918b7a118d0dee3e9ae4a20ac
ChaimaHedhili <70862886+ChaimaHedhili@users.noreply.github.com>
2022-03-28 01:47:49 +0200

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -30,38 +30,5 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: err.message })
       }
   });
-  //specific post
-  //router.get('/:postId', async (req, res) => {
-    // try{
-      //   const post = await Post.findById(req.params.postId);
-       //  res.json(post);
-     //}catch(err){
-       //  res.json({message: err});
-     //}
-    //});
-    //delete posts
-    //router.delete('/:postId', async (req, res) => {
-      //try{
-      //const removedPost = await Post.remove({_id: req.params.postId}) 
-       // res.json(removedPost);
-    //}catch(err){
-      //  res.json({message: err});
 
-    //}
-   
-   // });
-
-    //update
-   // router.patch('/:postId', async (req, res) => {
-     //   try {
-       // const updatedPost = await Post.updateOne(
-         //   {_id: req.params.postId},
-           // { $set: { title: req.body.title }}
-            //);
-          //res.json(updatedPost);
-    //  }catch(err){
-      //    res.json({message: err});
-      //}
-     
-     // });
   module.exports = router;
